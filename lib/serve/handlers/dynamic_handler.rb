974ed5a755ab0e8a147a69e76286c315bb263987
John W. Long <me@johnwlong.com>
2010-05-13 18:03:03 -0400

    Added additional view helpers for escaping content, access to params and flash, and creating tags in code

--- lib/serve/handlers/dynamic_handler.rb
+++ lib/serve/handlers/dynamic_handler.rb
@@ -1,3 +1,5 @@
+require 'serve/view_helpers'
+
 module Serve #:nodoc:
   class DynamicHandler < FileTypeHandler #:nodoc:
     extension 'erb', 'html.erb', 'rhtml', 'haml', 'html.haml'
@@ -105,106 +107,7 @@ module Serve #:nodoc:
         @erbout
       end
       
-      # This is extracted from Rails
-      def capture_erb(&block)
-        buffer = _erbout
-        pos = buffer.length
-        block.call
-        
-        # extract the block 
-        data = buffer[pos..-1]
-        
-        # replace it in the original with empty string
-        buffer[pos..-1] = ''
-        
-        data
-      end
-      
-      # Content_for methods
-      
-      def content_for(symbol, &block)
-        if @haml_buffer
-          set_content_for(symbol, capture_haml(&block))
-        else
-          set_content_for(symbol, capture_erb(&block))
-        end
-      end
-      
-      def content_for?(symbol)
-        !(get_content_for(symbol)).nil?
-      end
-      
-      def get_content_for(symbol = :content)
-        if symbol.to_s.intern == :content
-          @content
-        else
-          instance_variable_get("@content_for_#{symbol}")
-        end
-      end
-      
-      # View helper methods
-      
-      HTML_ESCAPE = { '&' => '&amp;',  '>' => '&gt;',   '<' => '&lt;', '"' => '&quot;' }
-      def html_escape(s)
-        s.to_s.gsub(/[&"><]/) { |special| HTML_ESCAPE[special] }
-      end
-      alias h html_escape
-      
-      def set_content_for(symbol, value)
-        instance_variable_set("@content_for_#{symbol}", value)
-      end
-      
-      def params
-        request.params
-      end
-      
-      # Render methods
-      
-      def render(options)
-        partial = options.delete(:partial)
-        template = options.delete(:template)
-        case
-        when partial
-          render_partial(partial)
-        when template
-          render_template(template)
-        else
-          raise "render options not supported #{options.inspect}"
-        end
-      end
-      
-      def render_partial(partial)
-        render_template(partial, :partial => true)
-      end
-      
-      def render_template(template, options={})
-        path = File.dirname(parser.script_filename)
-        if template =~ %r{^/}
-          template = template[1..-1]
-          path = @root_path
-        end
-        filename = template_filename(File.join(path, template), :partial => options.delete(:partial))
-        if File.file?(filename)
-          parser.parse_file(filename)
-        else
-          raise "File does not exist #{filename.inspect}"
-        end
-      end
-      
-      private
-      
-        def template_filename(name, options)
-          path = File.dirname(name)
-          template = File.basename(name)
-          template = "_" + template if options.delete(:partial)
-          template += extname(parser.script_filename) unless name =~ /\.[a-z]{3,4}$/
-          File.join(path, template)
-        end
-        
-        def extname(filename)
-          /(\.[a-z]{3,4}\.[a-z]{3,4})$/.match(filename)
-          $1 || File.extname(filename) || ''
-        end
+      include Serve::ViewHelpers
     end
   end
 end
---
+++ lib/serve/view_helpers.rb
@@ -0,0 +1,274 @@
+module Serve #:nodoc:
+  # Many of the methods here have been extracted from Rails
+  
+  module EscapeHelpers
+    HTML_ESCAPE = { '&' => '&amp;',  '>' => '&gt;',   '<' => '&lt;', '"' => '&quot;' }
+    JSON_ESCAPE = { '&' => '\u0026', '>' => '\u003E', '<' => '\u003C' }
+    
+    # A utility method for escaping HTML tag characters.
+    # This method is also aliased as <tt>h</tt>.
+    #
+    # In your ERb templates, use this method to escape any unsafe content. For example:
+    #   <%=h @person.name %>
+    #
+    # ==== Example:
+    #   puts html_escape("is a > 0 & a < 10?")
+    #   # => is a &gt; 0 &amp; a &lt; 10?
+    def html_escape(s)
+      s.to_s.gsub(/[&"><]/) { |special| HTML_ESCAPE[special] }
+    end
+    alias h html_escape
+    
+    # A utility method for escaping HTML entities in JSON strings.
+    # This method is also aliased as <tt>j</tt>.
+    #
+    # In your ERb templates, use this method to escape any HTML entities:
+    #   <%=j @person.to_json %>
+    #
+    # ==== Example:
+    #   puts json_escape("is a > 0 & a < 10?")
+    #   # => is a \u003E 0 \u0026 a \u003C 10?
+    def json_escape(s)
+      s.to_s.gsub(/[&"><]/) { |special| JSON_ESCAPE[special] }
+    end
+    
+    alias j json_escape
+  end
+  
+  module ContentHelpers
+    def capture_erb(&block)
+      buffer = _erbout
+      pos = buffer.length
+      block.call
+      
+      # extract the block 
+      data = buffer[pos..-1]
+      
+      # replace it in the original with empty string
+      buffer[pos..-1] = ''
+      
+      data
+    end
+    
+    def content_for(symbol, &block)
+      if @haml_buffer
+        set_content_for(symbol, capture_haml(&block))
+      else
+        set_content_for(symbol, capture_erb(&block))
+      end
+    end
+    
+    def content_for?(symbol)
+      !(get_content_for(symbol)).nil?
+    end
+    
+    def get_content_for(symbol = :content)
+      if symbol.to_s.intern == :content
+        @content
+      else
+        instance_variable_get("@content_for_#{symbol}")
+      end
+    end
+    
+    def set_content_for(symbol, value)
+      instance_variable_set("@content_for_#{symbol}", value)
+    end
+  end
+  
+  module FlashHelpers
+    def flash
+      @flash ||= {}
+    end
+  end
+  
+  module ParamHelpers
+    
+    # Key based access to query parameters. Keys can be strings or symbols.
+    def params
+      @params ||= HashWithIndifferentAccess.new(request.query)
+    end
+    
+    # Extract the value for a bool param. Handy for rendering templates in
+    # different states.
+    def boolean_param(key, default = false)
+      key = key.to_s.intern
+      value = params[key]
+      return default if value.blank?
+      case value.strip.downcase
+        when 'true', '1'  : true
+        when 'false', '0' :  false
+        else raise 'Invalid value'
+      end
+    end
+  end
+  
+  module RenderHelpers
+    def render(options)
+      partial = options.delete(:partial)
+      template = options.delete(:template)
+      case
+      when partial
+        render_partial(partial)
+      when template
+        render_template(template)
+      else
+        raise "render options not supported #{options.inspect}"
+      end
+    end
+    
+    def render_partial(partial)
+      render_template(partial, :partial => true)
+    end
+    
+    def render_template(template, options={})
+      path = File.dirname(parser.script_filename)
+      if template =~ %r{^/}
+        template = template[1..-1]
+        path = @root_path
+      end
+      filename = template_filename(File.join(path, template), :partial => options.delete(:partial))
+      if File.file?(filename)
+        parser.parse_file(filename)
+      else
+        raise "File does not exist #{filename.inspect}"
+      end
+    end
+    
+    private
+      
+      def template_filename(name, options)
+        path = File.dirname(name)
+        template = File.basename(name)
+        template = "_" + template if options.delete(:partial)
+        template += extname(parser.script_filename) unless name =~ /\.[a-z]{3,4}$/
+        File.join(path, template)
+      end
+      
+      def extname(filename)
+        /(\.[a-z]{3,4}\.[a-z]{3,4})$/.match(filename)
+        $1 || File.extname(filename) || ''
+      end
+  end
+  
+  module TagHelpers
+    def content_tag(name, content, html_options={})
+      %{<#{name}#{html_attributes(html_options)}>#{content}</#{name}>}
+    end
+    
+    def tag(name, html_options={})
+      %{<#{name}#{html_attributes(html_options)} />}
+    end
+    
+    def image_tag(src, html_options = {})
+      tag(:img, html_options.merge({:src=>src}))
+    end
+    
+    def image(name, options = {})
+      image_tag(append_image_extension("/images/admin/#{name}"), options)
+    end
+    
+    def javascript_tag(content = nil, html_options = {})
+      content_tag(:script, javascript_cdata_section(content), html_options.merge(:type => "text/javascript"))
+    end
+    
+    def link_to(name, href, html_options = {})
+      html_options = html_options.stringify_keys
+      confirm = html_options.delete("confirm")
+      onclick = "if (!confirm('#{html_escape(confirm)}')) return false;" if confirm
+      content_tag(:a, name, html_options.merge(:href => href, :onclick=>onclick))
+    end
+    
+    def link_to_function(name, *args, &block)
+      html_options = {}
+      html_options = args.pop if args.last.is_a? Hash
+      function = args[0] || ''
+      onclick = "#{"#{html_options[:onclick]}; " if html_options[:onclick]}#{function}; return false;"
+      href = html_options[:href] || '#'
+      content_tag(:a, name, html_options.merge(:href => href, :onclick => onclick))
+    end
+    
+    def mail_to(email_address, name = nil, html_options = {})
+      html_options = html_options.stringify_keys
+      encode = html_options.delete("encode").to_s
+      cc, bcc, subject, body = html_options.delete("cc"), html_options.delete("bcc"), html_options.delete("subject"), html_options.delete("body")
+      
+      string = ''
+      extras = ''
+      extras << "cc=#{CGI.escape(cc).gsub("+", "%20")}&" unless cc.nil?
+      extras << "bcc=#{CGI.escape(bcc).gsub("+", "%20")}&" unless bcc.nil?
+      extras << "body=#{CGI.escape(body).gsub("+", "%20")}&" unless body.nil?
+      extras << "subject=#{CGI.escape(subject).gsub("+", "%20")}&" unless subject.nil?
+      extras = "?" << extras.gsub!(/&?$/,"") unless extras.empty?
+      
+      email_address = email_address.to_s
+      
+      email_address_obfuscated = email_address.dup
+      email_address_obfuscated.gsub!(/@/, html_options.delete("replace_at")) if html_options.has_key?("replace_at")
+      email_address_obfuscated.gsub!(/\./, html_options.delete("replace_dot")) if html_options.has_key?("replace_dot")
+      
+      if encode == "javascript"
+        "document.write('#{content_tag("a", name || email_address_obfuscated, html_options.merge({ "href" => "mailto:"+email_address+extras }))}');".each_byte do |c|
+          string << sprintf("%%%x", c)
+        end
+        "<script type=\"#{Mime::JS}\">eval(decodeURIComponent('#{string}'))</script>"
+      elsif encode == "hex"
+        email_address_encoded = ''
+        email_address_obfuscated.each_byte do |c|
+          email_address_encoded << sprintf("&#%d;", c)
+        end
+        
+        protocol = 'mailto:'
+        protocol.each_byte { |c| string << sprintf("&#%d;", c) }
+        
+        email_address.each_byte do |c|
+          char = c.chr
+          string << (char =~ /\w/ ? sprintf("%%%x", c) : char)
+        end
+        content_tag "a", name || email_address_encoded, html_options.merge({ "href" => "#{string}#{extras}" })
+      else
+        content_tag "a", name || email_address_obfuscated, html_options.merge({ "href" => "mailto:#{email_address}#{extras}" })
+      end
+    end
+    
+    private
+      
+      def cdata_section(content)
+        "<![CDATA[#{content}]]>"
+      end
+      
+      def javascript_cdata_section(content) #:nodoc:
+        "\n//#{cdata_section("\n#{content}\n//")}\n"
+      end
+      
+      def html_attributes(options)
+        unless options.blank?
+          attrs = []
+          options.each_pair do |key, value|
+            if value == true
+              attrs << %(#{key}="#{key}") if value
+            else
+              attrs << %(#{key}="#{value}") unless value.nil?
+            end
+          end
+          " #{attrs.sort * ' '}" unless attrs.empty?
+        end
+      end
+      
+      def append_image_extension(name)
+        unless name =~ /\.(.*?)$/
+          name + '.png'
+        else
+          name
+        end
+      end
+  end
+  
+  module ViewHelpers #:nodoc:
+    include EscapeHelpers
+    include ContentHelpers
+    include FlashHelpers
+    include ParamHelpers
+    include RenderHelpers
+    include TagHelpers
+  end
+end
