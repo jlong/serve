c9f93b4882dca7a93b8f900cfb21f78db1bdf66f
John W. Long <me@johnwlong.com>
2011-06-25 01:02:17 -0400

    remove obsolete rails plugin initializer

--- rails/init.rb
+++
@@ -1,6 +0,0 @@
-require 'serve/rails'
-
-ActionController::Routing::RouteSet::Mapper.send :include, Serve::Rails::Routing::MapperExtensions
-ActionController::AbstractResponse.module_eval do
-  attr_accessor :cache_timeout
-end
