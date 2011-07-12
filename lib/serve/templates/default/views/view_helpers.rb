c2741b08ab4b5d5941b9d9d8c7044ff4ae472275
John W. Long <me@johnwlong.com>
2011-07-12 14:00:03 -0400

    whoops! image tag wasn't working

--- lib/serve/templates/default/views/view_helpers.rb
+++ lib/serve/templates/default/views/view_helpers.rb
@@ -5,18 +5,6 @@
 #
 module ViewHelpers
   
-  # Handy for hiding a block of unfinished code
-  def hidden(&block)
-    #no-op
-  end
-  
-  # Shorthand for referencing images in the images folder
-  def image(name, options = {})
-    path = "/images/\#{name}"
-    path += ".png" unless path.match(/\.[A-za-z]{3,4}$/)
-    image_tag(name, {:alt => ""}.update(options))
-  end
-  
   # Calculate the years for a copyright
   def copyright_years(start_year)
     end_year = Date.today.year
@@ -26,5 +14,12 @@ module ViewHelpers
       "\#{start_year}&#8211;\#{end_year}"
     end
   end
+  
+  # Handy for hiding a block of unfinished code
+  def hidden(&block)
+    #no-op
+  end
+  
+  # Add your own helpers below...
   
 end
