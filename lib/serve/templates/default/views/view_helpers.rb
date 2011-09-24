8f8840adeff36a7a8335244c4f025ce0797cfb01
Oskar L-B <oskar@OLBproductions.com>
2011-09-24 16:28:38 +0200

    Unescaped \# in interpolated copyright_years helper

--- lib/serve/templates/default/views/view_helpers.rb
+++ lib/serve/templates/default/views/view_helpers.rb
@@ -9,9 +9,9 @@ module ViewHelpers
   def copyright_years(start_year)
     end_year = Date.today.year
     if start_year == end_year
-      "\#{start_year}"
+      "#{start_year}"
     else
-      "\#{start_year}&#8211;\#{end_year}"
+      "#{start_year}&#8211;#{end_year}"
     end
   end
   
